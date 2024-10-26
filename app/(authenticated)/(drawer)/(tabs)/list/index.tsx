c1b80251be533a69a173cc8444687c7e6e6c755e
bienlim <bienlim.dev@gmail.com>
2024-10-26 21:07:40 +0800

    feat: added todos API

--- app/(authenticated)/(drawer)/(tabs)/list/index.tsx
+++ app/(authenticated)/(drawer)/(tabs)/list/index.tsx
@@ -1,22 +1,56 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { useAuth } from "@/context/AuthContext";
 import { Link } from "expo-router";
+import { useEffect, useState } from "react";
+import { View, Text, Button, FlatList, TouchableOpacity } from "react-native";
+const Page = () => {
+	const { token } = useAuth();
+	console.log("list token  ", token);
+	const [todos, setTodos] = useState([]);
 
-const index = () => {
+	useEffect(() => {
+		loadTodos();
+	}, []);
+
+	const loadTodos = async () => {
+		const response = await fetch("/api/todos", {
+			headers: {
+				Authorization: `${token}`,
+			},
+		});
+
+		const data = await response.json();
+		setTodos(data);
+	};
+
 	return (
 		<View style={{ flex: 1 }}>
-			<Text>list</Text>
+			<Text>List</Text>
 			<Link href={"/(authenticated)/(drawer)/(tabs)/list/42"} asChild>
 				<Button title="Go to 42" />
 			</Link>
 			<Link href={"/(authenticated)/(drawer)/(tabs)/list/5?query=foo"} asChild>
 				<Button title="Go to 5" />
 			</Link>
-			<Link href={"/(authenticated)/details/1337?query=Outside"} asChild>
+
+			<Link href={"/(authenticated)/details/1337?query=whatever"} asChild>
 				<Button title="Go to 1337 outside" />
 			</Link>
+
+			<FlatList
+				data={todos}
+				keyExtractor={(item: any) => item._id}
+				renderItem={({ item }) => (
+					<Link
+						href={`/(authenticated)/(drawer)/(tabs)/list/${item._id}`}
+						asChild
+					>
+						<TouchableOpacity style={{ padding: 10, height: 44 }}>
+							<Text>{item.task}</Text>
+						</TouchableOpacity>
+					</Link>
+				)}
+			/>
 		</View>
 	);
 };
-
-export default index;
+export default Page;
